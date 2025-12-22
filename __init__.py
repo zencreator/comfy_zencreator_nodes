9ca9409b75d39b4b0329d8771a87da3de963c80c
kostyanchik94 <113383707+kostyanchik94@users.noreply.github.com>
2025-12-23 01:21:35 +0700

    Update __init__.py убрал ноду бананы через фал аи
    убрал ноду бананы через фал аи

--- __init__.py
+++ __init__.py
@@ -7,7 +7,7 @@ BytePlus Seedream 4.0 & Fal.ai & Gemini 3 Pro
 from .BytePlusSeedreamNode import NODE_CLASS_MAPPINGS as BytePlus_Classes, NODE_DISPLAY_NAME_MAPPINGS as BytePlus_Names
 
 # 2. Импортируем маппинги из второго файла (Fal.ai)
-from .NanoBanabaFALAI import NODE_CLASS_MAPPINGS as Fal_Classes, NODE_DISPLAY_NAME_MAPPINGS as Fal_Names
+#from .NanoBanabaFALAI import NODE_CLASS_MAPPINGS as Fal_Classes, NODE_DISPLAY_NAME_MAPPINGS as Fal_Names
 
 # 3. Импортируем маппинги из НОВОГО файла (Gemini)
 # Убедитесь, что имя файла (без .py) совпадает с тем, что вы создали на Шаге 1
@@ -16,13 +16,13 @@ from .NanoBananaGemini import NODE_CLASS_MAPPINGS as Gemini_Classes, NODE_DISPLA
 # 4. Объединяем все словари в общие переменные
 NODE_CLASS_MAPPINGS = {
     **BytePlus_Classes, 
-    **Fal_Classes, 
+#    **Fal_Classes, 
     **Gemini_Classes
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     **BytePlus_Names, 
-    **Fal_Names, 
+#    **Fal_Names, 
     **Gemini_Names
 }
 
